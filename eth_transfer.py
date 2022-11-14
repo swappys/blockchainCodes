1a94cb310e8962fcf8e80c4720c740f22edb92e8
swapnil <swapnilvernekar04@gmail.com>
2022-11-14 16:23:12 +0000

    initial commit

---
+++ eth_transfer.py
@@ -0,0 +1,29 @@
+from web3 import Web3
+from decouple import config
+
+infuraurl = config('INFURA_URL')
+ownerAddress=config('OWNER_ADDRESS')
+contractAddress=config('CONTRACT_ADDRESS')
+Private_Key=config('SUPER_SECRET_PRIVATE_KEY')
+abi='[{"inputs":[{"internalType":"string","name":"name_","type":"string"},{"internalType":"string","name":"symbol_","type":"string"}],"stateMutability":"nonpayable","type":"constructor"},{"anonymous":false,"inputs":[{"indexed":true,"internalType":"address","name":"owner","type":"address"},{"indexed":true,"internalType":"address","name":"spender","type":"address"},{"indexed":false,"internalType":"uint256","name":"value","type":"uint256"}],"name":"Approval","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"internalType":"address","name":"from","type":"address"},{"indexed":true,"internalType":"address","name":"to","type":"address"},{"indexed":false,"internalType":"uint256","name":"value","type":"uint256"}],"name":"Transfer","type":"event"},{"inputs":[{"internalType":"address","name":"owner","type":"address"},{"internalType":"address","name":"spender","type":"address"}],"name":"allowance","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"address","name":"spender","type":"address"},{"internalType":"uint256","name":"amount","type":"uint256"}],"name":"approve","outputs":[{"internalType":"bool","name":"","type":"bool"}],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"address","name":"account","type":"address"}],"name":"balanceOf","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"decimals","outputs":[{"internalType":"uint8","name":"","type":"uint8"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"address","name":"spender","type":"address"},{"internalType":"uint256","name":"subtractedValue","type":"uint256"}],"name":"decreaseAllowance","outputs":[{"internalType":"bool","name":"","type":"bool"}],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"address","name":"spender","type":"address"},{"internalType":"uint256","name":"addedValue","type":"uint256"}],"name":"increaseAllowance","outputs":[{"internalType":"bool","name":"","type":"bool"}],"stateMutability":"nonpayable","type":"function"},{"inputs":[],"name":"name","outputs":[{"internalType":"string","name":"","type":"string"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"symbol","outputs":[{"internalType":"string","name":"","type":"string"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"totalSupply","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"address","name":"to","type":"address"},{"internalType":"uint256","name":"amount","type":"uint256"}],"name":"transfer","outputs":[{"internalType":"bool","name":"","type":"bool"}],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"address","name":"from","type":"address"},{"internalType":"address","name":"to","type":"address"},{"internalType":"uint256","name":"amount","type":"uint256"}],"name":"transferFrom","outputs":[{"internalType":"bool","name":"","type":"bool"}],"stateMutability":"nonpayable","type":"function"}]'
+web3=Web3(Web3.HTTPProvider(infuraurl))
+if web3.isConnected():
+    print ('CONNECTED TO WEB3')
+    amountInEther = "0.001"
+    targetAddress = "0x4b69BEe3D41C6ff232850E844542eC9C8654E7EE"
+    nonce=web3.eth.getTransactionCount(ownerAddress)
+    print(nonce)
+    gasPrice=web3.toWei('100','gwei')
+    value=web3.toWei(amountInEther,'ether')
+    tx= {'nonce':nonce,
+    'to':targetAddress,
+    'value':value,
+    'gas':21000,
+    'gasPrice':gasPrice,
+    'chainId':5}
+    signed_tx=web3.eth.account.sign_transaction(tx,Private_Key)
+    tx_hash=web3.eth.send_raw_transaction(signed_tx.rawTransaction)
+    print('tx hash: '+tx_hash.hex())
+
+    web3.eth.waitForTransactionReceipt(tx_hash)
+    print('tx mined')
---
+++ test.py
@@ -0,0 +1,46 @@
+from web3 import Web3
+from decouple import config
+
+# Testing the base python app
+print("Welcome to Blockchain using python")
+
+# testing the .env file
+infuraUrl = config('INFURA_URL')
+print('Infura url: ' + infuraUrl)
+
+# This is the address of your ERC20 Contract (0xblablah)
+contractAddress = config('CONTRACT_ADDRESS')
+
+# This is your primary metamask address (0xblablah)
+ownerAddress = config('OWNER_ADDRESS')
+
+# This is your private key (from metamask - large hex number)
+privateKey = config('SUPER_SECRET_PRIVATE_KEY')
+
+# This is like API but for blockchain
+abi = '[{"inputs":[{"internalType":"string","name":"name_","type":"string"},{"internalType":"string","name":"symbol_","type":"string"}],"stateMutability":"nonpayable","type":"constructor"},{"anonymous":false,"inputs":[{"indexed":true,"internalType":"address","name":"owner","type":"address"},{"indexed":true,"internalType":"address","name":"spender","type":"address"},{"indexed":false,"internalType":"uint256","name":"value","type":"uint256"}],"name":"Approval","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"internalType":"address","name":"from","type":"address"},{"indexed":true,"internalType":"address","name":"to","type":"address"},{"indexed":false,"internalType":"uint256","name":"value","type":"uint256"}],"name":"Transfer","type":"event"},{"inputs":[{"internalType":"address","name":"owner","type":"address"},{"internalType":"address","name":"spender","type":"address"}],"name":"allowance","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"address","name":"spender","type":"address"},{"internalType":"uint256","name":"amount","type":"uint256"}],"name":"approve","outputs":[{"internalType":"bool","name":"","type":"bool"}],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"address","name":"account","type":"address"}],"name":"balanceOf","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"decimals","outputs":[{"internalType":"uint8","name":"","type":"uint8"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"address","name":"spender","type":"address"},{"internalType":"uint256","name":"subtractedValue","type":"uint256"}],"name":"decreaseAllowance","outputs":[{"internalType":"bool","name":"","type":"bool"}],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"address","name":"spender","type":"address"},{"internalType":"uint256","name":"addedValue","type":"uint256"}],"name":"increaseAllowance","outputs":[{"internalType":"bool","name":"","type":"bool"}],"stateMutability":"nonpayable","type":"function"},{"inputs":[],"name":"name","outputs":[{"internalType":"string","name":"","type":"string"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"symbol","outputs":[{"internalType":"string","name":"","type":"string"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"totalSupply","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"address","name":"to","type":"address"},{"internalType":"uint256","name":"amount","type":"uint256"}],"name":"transfer","outputs":[{"internalType":"bool","name":"","type":"bool"}],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"address","name":"from","type":"address"},{"internalType":"address","name":"to","type":"address"},{"internalType":"uint256","name":"amount","type":"uint256"}],"name":"transferFrom","outputs":[{"internalType":"bool","name":"","type":"bool"}],"stateMutability":"nonpayable","type":"function"}]'
+
+# Connecting to Web3
+web3 = Web3(Web3.HTTPProvider(infuraUrl))
+
+res = web3.isConnected()
+
+# Check if the connection is established
+if res:
+    print("Connected to Web3")
+
+    # Connect to your contract, using its address and ABI
+    contract_instance=web3.eth.contract(address=contractAddress,abi=abi)
+    
+    # Query your contract
+    symbol = contract_instance.functions.symbol().call()
+    print("My Contract Symbol is: "+symbol)
+
+    decimals = contract_instance.functions.decimals().call()
+    print("My Contract Decimals is: "+ str(decimals))
+
+    balance = contract_instance.functions.balanceOf(ownerAddress).call()
+    print("My Contract Balance is: "+ str(balance))
+
+else:
+    print("Connection Failed!")
